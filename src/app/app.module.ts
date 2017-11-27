a3815f53908dcf1e3919dff23a0ea3cebcf06b84
= <jimmy@eoxia.com>
2017-11-27 21:29:29 +0100

    0.1.0: Ajout composants, services
    Composants: Menu, Apps
    Services: Auth, App.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -14,10 +14,18 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { ElectronService } from './providers/electron.service';
 
+import { MenuComponent } from './components/menu/menu.component';
+import { AppsComponent } from './components/apps/apps.component';
+
+import { AppService } from './services/app/app.service';
+import { AuthService } from './services/auth/auth.service';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    AppsComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +34,7 @@ import { ElectronService } from './providers/electron.service';
     AppRoutingModule,
 		HttpClientModule
   ],
-  providers: [ElectronService],
+  providers: [ElectronService, AppService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
